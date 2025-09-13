a7c4dd553e4d511b059a12986a584fa6ae02282d
studiowalling <studiowalling@gmail.com>
2025-09-13 18:49:02 +0530

    redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'setlite.in', // when someone visits setlite.in
+          },
+        ],
+        destination: 'https://setlite.com/:path*', // send them to setlite.com
+        permanent: true, // 308 Permanent Redirect
+      },
+    ];
+  },
+};
 
 export default nextConfig;
